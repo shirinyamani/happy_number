f0a20c4b40eb5f38d3b70692d23a5b79f420ee7a
shirinyamani <yamani.shirin@ucalgary.ca>
2024-01-15 12:20:41 -0700

    happy numbers documentation added

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 def happy_num(num):
+    """
+    A happy numbers is a number defined by the following process:
+    Starting with any positive integer, replace the number by the sum of the
+    squares of its digits, and repeat the process until the number equals
+    1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+
+    :param n: Input number
+    :return: True if n is a happy number, False otherwise
+
+    Examples:
+
+    >>> is_happy(19)
+    True
+
+    >>> is_happy(2)
+    False
+
+    >>> is_happy(44)
+    True
+    """
+    
     seen = set()
     while (num != 1) and (num not in seen):
         seen.add(num)
